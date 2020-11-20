2be92f9c455d4df9fb54e43695f4f12762fe6940
Credv <59005369+Credv@users.noreply.github.com>
2020-11-20 11:52:29 +0800

    init server

---
+++ public/precache-manifest.1199e45f8276bfd1906e80f570f37eac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7542a03083f51bc1c85740877dfd731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a96b6b08f1869582849",
+    "url": "/static/css/2.de1f55db.chunk.css"
+  },
+  {
+    "revision": "74784ad6c5d4db9956c7",
+    "url": "/static/css/main.80b96a23.chunk.css"
+  },
+  {
+    "revision": "2a96b6b08f1869582849",
+    "url": "/static/js/2.3042cb98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3042cb98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74784ad6c5d4db9956c7",
+    "url": "/static/js/main.595f1af4.chunk.js"
+  },
+  {
+    "revision": "3f4f3f60835993740eeb",
+    "url": "/static/js/runtime-main.15b5ebad.js"
+  },
+  {
+    "revision": "ade72bc20202e9372534434cb6c31a52",
+    "url": "/static/media/bg.ade72bc2.jpg"
+  },
+  {
+    "revision": "766af9fcbe3706134019c50107313dfe",
+    "url": "/static/media/logo.766af9fc.png"
+  }
+]);
